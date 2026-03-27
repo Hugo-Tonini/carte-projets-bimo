d1aed86e5f17b43a49f4d505e7406d0dc57b94cc
Hugo-Tonini <hugocl@live.fr>
2026-03-27 16:35:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -222,7 +222,7 @@
     chunkedLoading: true,
     chunkInterval: 10,
     spiderfyOnMaxZoom: true,
-    maxClusterRadius: 30,
+    maxClusterRadius: 20,
     spiderfyDistanceMultiplier: 1.3,
     showCoverageOnHover: false,
     zoomToBoundsOnClick: true,
