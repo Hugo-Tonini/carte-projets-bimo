e80d395f3b1e5c6544b27aef9506685d5c71f34b
Hugo-Tonini <hugocl@live.fr>
2026-04-22 09:51:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -83,7 +83,7 @@
     "Nord-Est": "#F59E0B",
     "Grand Sud-Ouest": "#10B981",
     "Alpes Centre-Est": "#8B5CF6",
-    "Méditerranée Grand-Sud": "#0EA5E9",
+    "Méditerranée Grand-Sud": "#0284C7",
     "Nord-Ouest Île-de-France": "#EF4444"
 };
 
